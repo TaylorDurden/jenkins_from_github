4858b2fb6025f3fc36aa7716d8c33de7df56cd90
TAYLOR-SKULL\taylorlee <258266864@qq.com>
2019-03-05 17:19:21 +0800

    port 8002

--- test_git/app.py
+++ test_git/app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello Shiyanlou002!Hello Shiyanlou003!Hello Shiyanlou004!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host="0.0.0.0", port=8002, debug=True)
